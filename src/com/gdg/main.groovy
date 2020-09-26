2803251c864231d06309d9bf99784293393115e9
dimasrizkyhp <dimas.rizky.hp@gmail.com>
2020-09-26 08:42:43 +0700

    initial commit

--- src/com/gdg/main.groovy
+++ src/com/gdg/main.groovy
@@ -5,11 +5,12 @@ def main(script) {
     // Pipeline specific variable get from injected env
     // Mandatory variable will be echeck at details & validation steps
     def String branch_name = ("${script.env.branch_name}" != "null") ? "${script.env.branch_name}" : ""
+    def String package_name = ("${script.env.package_name}" != "null") ? "${script.env.package_name}" : ""
 
     ansiColor('xterm') {
         stage('Git Checkout') {
             println("==================Cloning Code==================")
-            git branch: "${branch_name}", url: "https://github.com/dimasmamot/${JOB_NAME}.git"
+            git branch: "${branch_name}", url: "https://github.com/dimasmamot/${package_name}.git"
             println("==================Success Cloning Code==================")
         }
         stage('Building') {
@@ -26,7 +27,7 @@ def main(script) {
                 sh 'rm -rf build-*.tar.gz'
                 sh "tar czf build-${BUILD_NUMBER}.tar.gz build/*"
                 sh 'tar czf build-latest.tar.gz build/*'
-                googleStorageUpload bucket: "gs://artifact-gdg-demo/$JOB_NAME", credentialsId: 'gdg-demo-290501', pattern: 'build-*.tar.gz'
+                googleStorageUpload bucket: "gs://artifact-gdg-demo/$package_name", credentialsId: 'gdg-demo-290501', pattern: 'build-*.tar.gz'
             }
         }
         stage('Deploy') {
@@ -39,7 +40,7 @@ def main(script) {
             withPythonEnv('System-CPython-2.7') {
                 sh 'pip install ansible'
                 dir('playbooks'){
-                    ansiblePlaybook colorized: true, credentialsId: '3cf031db-f7b1-4c50-b58b-7d36b3c69718', disableHostKeyChecking: true, extras: "-e 'pkg_name=$JOB_NAME' -e 'pkg_version=$BUILD_NUMBER'", installation: 'ansible-workspace', inventory: '../inventories/gcp/gdg-demo/asia-southeast2/production/inventory.ini', playbook: "${JOB_NAME}.yml", tags: 'update'
+                    ansiblePlaybook colorized: true, credentialsId: '3cf031db-f7b1-4c50-b58b-7d36b3c69718', disableHostKeyChecking: true, extras: "-e 'pkg_name=$package_name' -e 'pkg_version=$BUILD_NUMBER'", installation: 'ansible-workspace', inventory: '../inventories/gcp/gdg-demo/asia-southeast2/production/inventory.ini', playbook: "${package_name}.yml", tags: 'update'
                 }
             }
         }
